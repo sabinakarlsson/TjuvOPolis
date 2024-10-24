719408726b7c46e7f1238cadc2dd44cbfc2fac10
Sabina Karlsson <karlsson.sst@gmail.com>
2024-10-24 21:11:07 +0200

    FLyttade funktionen "bokstäver möts" till en egen klass + metod. För att snygga till lite och hitta lättare

--- TjuvOPolis/City.cs
+++ TjuvOPolis/City.cs
@@ -54,5 +54,120 @@ namespace TjuvOPolis
                 Console.WriteLine();
             }
         }
+
+
+
+
+        public static void MeetingLetters(string[,] myCity, List<Person> myTown, string[,] myPrison, List<Person> myPrisoners, Arrest arrest, Robbed robbed)
+        {
+
+
+            // Kolla om någon bokstav möts
+            for (int i = 0; i < myTown.Count; i++)
+            {
+                for (int j = i + 1; j < myTown.Count; j++)
+                {
+                    if (myTown[i].PlacementY == myTown[j].PlacementY && myTown[i].PlacementX == myTown[j].PlacementX)
+                    {
+                        //om polisRaden möter tjuvKolumnen
+                        if (myTown[i] is Police && myTown[j] is Thief)
+                        {
+                            Console.Write(((Police)myTown[i]).Name + " och " + ((Thief)myTown[j]).Name + " möttes.");
+                            if (((Thief)myTown[j]).StolenProperty.Count > 0)
+                            {
+                                Police.Confiscate((Police)myTown[i], (Thief)myTown[j]);
+                                arrest.ShowArrest((Thief)myTown[j]);
+                                myTown.RemoveAt(j);
+                                myPrisoners.Add(myTown[j]);
+                                Console.WriteLine();
+                            }
+
+                            else if (((Thief)myTown[j]).StolenProperty.Count == 0)
+                            {
+                                Console.WriteLine(" Polisen genomsökte " + ((Thief)myTown[j]).Name + ", men hen hade inga stulna värdesaker på sig (denna gång..)");
+                                Console.WriteLine();
+                            }
+                        }
+
+                        //NÖDVÄNDIG?? Exakt som ovan men istället om tjuvRaden möter polisKolumnen
+                        if (myTown[j] is Police && myTown[i] is Thief)
+                        {
+                            Console.Write(((Police)myTown[j]).Name + " och " + ((Thief)myTown[i]).Name + " möttes.");
+
+                            if (((Thief)myTown[i]).StolenProperty.Count > 0)
+                            {
+                                Police.Confiscate((Police)myTown[j], (Thief)myTown[i]);
+                                arrest.ShowArrest((Thief)myTown[i]);
+                                myTown.RemoveAt(i);
+                                myPrisoners.Add((Thief)myTown[i]);
+                                Console.WriteLine();
+                            }
+
+                            else if (((Thief)myTown[i]).StolenProperty.Count == 0)
+                            {
+                                Console.WriteLine(" Polisen genomsökte " + ((Thief)myTown[i]).Name + ", men hen hade inga stulna värdesaker på sig (denna gång..)");
+                                Console.WriteLine();
+                            }
+                        }
+
+                        //om tjuvRaden möter medborgarKolumnen
+                        if (myTown[i] is Thief && myTown[j] is Citizen)
+                        {
+                            Console.Write(((Thief)myTown[i]).Name + " och " + ((Citizen)myTown[j]).Name + " möttes.");
+
+                            if (((Citizen)myTown[j]).PropertyInPossession.Count > 0)
+                            {
+                                Thief.Steal((Citizen)myTown[j], (Thief)myTown[i]);
+                                robbed.ShowRobbed((Thief)myTown[i]);
+                                Console.WriteLine();
+                            }
+
+                            else if (((Citizen)myTown[j]).PropertyInPossession.Count == 0)
+                            {
+                                Console.WriteLine(((Thief)myTown[i]).Name + " försökte råna " + ((Citizen)myTown[j]).Name + ", men hen har redan blivit bestulen på alla sina värdesaker..");
+                                Console.WriteLine();
+                            }
+                        }
+
+                        //NÖDVÄNDIG?? Exakt som ovan men istället om tjuvKolumnen möter medborgarRaden
+                        if (myTown[j] is Thief && myTown[i] is Citizen)
+                        {
+                            Console.Write(((Thief)myTown[j]).Name + " och " + ((Citizen)myTown[i]).Name + " möttes.");
+
+                            if (((Citizen)myTown[i]).PropertyInPossession.Count > 0)
+                            {
+                                Thief.Steal((Citizen)myTown[i], (Thief)myTown[j]);
+                                robbed.ShowRobbed((Thief)myTown[j]);
+                                Console.WriteLine();
+                            }
+
+                            else if (((Citizen)myTown[i]).PropertyInPossession.Count == 0)
+                            {
+                                Console.WriteLine(((Thief)myTown[j]).Name + " försökte råna " + ((Citizen)myTown[i]).Name + ", men hen har redan blivit bestulen på alla sina värdesaker..");
+                                Console.WriteLine();
+                            }
+                        }
+
+                        //om medborgarRaden möter polisKolumnen
+                        if (myTown[i] is Citizen && myTown[j] is Police)
+                        {
+                            Console.WriteLine(((Citizen)myTown[i]).Name + " och " + ((Police)myTown[j]).Name + " möttes. De hälsade på varandra");
+                            Console.WriteLine();
+                        }
+
+                        //NÖDVÄNDIG?? Exakt som ovan men istället om medborgarKolumnen möter polisRaden
+                        if (myTown[j] is Citizen && myTown[i] is Police)
+                        {
+                            Console.WriteLine(((Citizen)myTown[j]).Name + " och " + ((Police)myTown[i]).Name + " möttes. De hälsade på varandra");
+                            Console.WriteLine();
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Antal gripna: " + arrest.NumberArrested);
+            Console.WriteLine("Antal rånade: " + robbed.NumberRobbed);
+        }
     }
 }
--- TjuvOPolis/Program.cs
+++ TjuvOPolis/Program.cs
@@ -52,9 +52,6 @@ namespace TjuvOPolis
 
                 //Sätter ut punkter i min stad, skriver ut bokstäver samt visar min stad:
                 City.ShowCity(myCity, myTown);
-
-
-                Console.WriteLine("-------------------------");
                 Console.WriteLine();
 
 
@@ -63,110 +60,14 @@ namespace TjuvOPolis
 
                 Console.WriteLine();
                 Console.WriteLine("-------------------------");
+
+                City.MeetingLetters(myCity, myTown, myPrison, myPrisoners, arrest, robbed);
+
                 
-
-
-                // Kolla om någon bokstav möts
-                for (int i = 0; i < myTown.Count; i++)
-                {
-                    for (int j = i + 1; j < myTown.Count; j++)
-                    {
-                        if (myTown[i].PlacementY == myTown[j].PlacementY && myTown[i].PlacementX == myTown[j].PlacementX)
-                        {
-                            //om polisraden möter tjuvkolumnen
-                            if (myTown[i] is Police && myTown[j] is Thief)
-                            {
-                                Console.Write(((Police)myTown[i]).Name + " och " + ((Thief)myTown[j]).Name + " möttes.");
-                                if (((Thief)myTown[j]).StolenProperty.Count > 0)
-                                {
-                                    Police.Confiscate((Police)myTown[i], (Thief)myTown[j]);
-                                    arrest.ShowArrest((Thief)myTown[j]);
-                                    myTown.RemoveAt(j);
-                                    myPrisoners.Add(myTown[j]);
-                                }
-
-                                else if (((Thief)myTown[j]).StolenProperty.Count == 0)
-                                {
-                                    Console.WriteLine(" Polisen genomsökte " + ((Thief)myTown[j]).Name + ", men hen hade inga stulna värdesaker på sig (denna gång..)");
-                                }
-                            }
-
-                            //NÖDVÄNDIG?? om tjuvraden möter poliskolumnen
-                            if (myTown[j] is Police && myTown[i] is Thief)
-                            {
-                                Console.Write(((Police)myTown[j]).Name + " och " + ((Thief)myTown[i]).Name + " möttes.");
-
-                                if (((Thief)myTown[i]).StolenProperty.Count > 0)
-                                {
-                                    Police.Confiscate((Police)myTown[j], (Thief)myTown[i]);
-                                    arrest.ShowArrest((Thief)myTown[i]);
-                                    myTown.RemoveAt(i);
-                                    myPrisoners.Add((Thief)myTown[i]);
-                                }
-
-                                else if (((Thief)myTown[i]).StolenProperty.Count == 0)
-                                {
-                                    Console.WriteLine(" Polisen genomsökte " + ((Thief)myTown[i]).Name + ", men hen hade inga stulna värdesaker på sig (denna gång..)");
-                                }
-                            }
-
-                            //om tjuvraden möter medborgarkolumnen
-                            if (myTown[i] is Thief && myTown[j] is Citizen)
-                            {
-                                Console.Write(((Thief)myTown[i]).Name + " och " + ((Citizen)myTown[j]).Name + " möttes.");
-
-                                if (((Citizen)myTown[j]).PropertyInPossession.Count > 0)
-                                {
-                                    Thief.Steal((Citizen)myTown[j], (Thief)myTown[i]);
-                                    robbed.ShowRobbed((Thief)myTown[i]);
-                                }
-
-                                else if (((Citizen)myTown[j]).PropertyInPossession.Count == 0)
-                                {
-                                    Console.WriteLine(((Thief)myTown[i]).Name + " försökte råna " + ((Citizen)myTown[j]).Name + ", men hen har redan blivit bestulen på alla sina värdesaker..");
-                                }
-                            }
-
-                            //NÖDVÄNDIG?? om tjuvkolumnen möter medborgarraden
-                            if (myTown[j] is Thief && myTown[i] is Citizen)
-                            {
-                                Console.Write(((Thief)myTown[j]).Name + " och " + ((Citizen)myTown[i]).Name + " möttes.");
-
-                                if (((Citizen)myTown[i]).PropertyInPossession.Count > 0)
-                                {
-                                    Thief.Steal((Citizen)myTown[i], (Thief)myTown[j]);
-                                    robbed.ShowRobbed((Thief)myTown[j]);
-                                }
-
-                                else if (((Citizen)myTown[i]).PropertyInPossession.Count == 0)
-                                {
-                                    Console.WriteLine(((Thief)myTown[j]).Name + " försökte råna " + ((Citizen)myTown[i]).Name + ", men hen har redan blivit bestulen på alla sina värdesaker..");
-                                }
-                            }
-
-                            //om medborgarraden möter poliskolumnen
-                            if (myTown[i] is Citizen && myTown[j] is Police)
-                            {
-                                Console.WriteLine(((Citizen)myTown[i]).Name + " och " + ((Police)myTown[j]).Name + " möttes. De hälsade på varandra");
-                            }
-
-                            //NÖDVÄNDIG?? om medborgarkolumnen möter polisraden
-                            if (myTown[j] is Citizen && myTown[i] is Police)
-                            {
-                                Console.WriteLine(((Citizen)myTown[j]).Name + " och " + ((Police)myTown[i]).Name + " möttes. De hälsade på varandra");
-                            }
-
-                        }
-                    }
-                }
-                
-                
-
 
                 Console.WriteLine();
-                Console.WriteLine("Antal gripna: " + arrest.NumberArrested);
-                Console.WriteLine("Antal rånade: " + robbed.NumberRobbed);
-                Thread.Sleep(1000);
+                Console.ReadLine();
+                //Thread.Sleep(1000);
 
             }
 
